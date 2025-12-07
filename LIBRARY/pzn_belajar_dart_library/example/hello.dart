5e0bf35608e27faddb6fb42018e0d468878b7e31
RIOKOWI <achyarrio2004@gmail.com>
2025-12-07 14:50:26 +0700

    MEMBUAT LIBRARY: contoh code import library

---
+++ LIBRARY/pzn_belajar_dart_library/example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:pzn_belajar_dart_library/hello.dart'; // import library
+
+void main(){
+  print(sayHello("rio"));
+  
+  var pelanggan = Customer();
+  var kategori = Category();
+  var produk = Product();
+}
